12c91e5ae552cc388a3f76bb4b680074426909bb
RamvigneshA <ramvigneshnanban@gmail.com>
2024-11-15 19:36:53 +0530

    made the public file with all the asserts and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
- 
 export default function Home() {
   return (
     <div className="flex-center h-screen">
-      <h1 className='h1'>
-        store it hjsdv ds dsh kus kskss
-      </h1>
- </div>
+      <h1 className="h1">store it hjsdv ds dsh kus kskss</h1>
+    </div>
   );
 }
